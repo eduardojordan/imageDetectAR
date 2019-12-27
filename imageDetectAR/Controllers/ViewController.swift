fda5cbbcaa6a36e330f470f82726851a76f611d9
MAC <>
2019-12-27 21:52:34 +0100

    add pending update

--- imageDetectAR/Controllers/ViewController.swift
+++ imageDetectAR/Controllers/ViewController.swift
@@ -7,14 +7,48 @@
 //
 
 import UIKit
+import ARKit
+
 
 class ViewController: UIViewController {
 
+    @IBOutlet var sceneView: ARSCNView!
+    
+    let updateQueue = DispatchQueue(label: "\(Bundle.main.bundleIdentifier!).serialSCNQueue")
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        sceneView.showsStatistics = true
+        
+        sceneView.delegate = self 
+        sceneView.autoenablesDefaultLighting = true
+        sceneView.automaticallyUpdatesLighting = true
+     
     }
 
+      override func viewWillAppear(_ animated: Bool) {
+          super.viewWillAppear(animated)
 
+          guard let refImages = ARReferenceImage.referenceImages(inGroupNamed: "AR Resources", bundle: Bundle.main) else {
+                  fatalError("Missing expected asset catalog resources.")
+          }
+
+          // Create a session configuration
+          let configuration = ARImageTrackingConfiguration()
+          configuration.trackingImages = refImages
+          configuration.maximumNumberOfTrackedImages = 1
+
+          // Run the view's session
+          sceneView.session.run(configuration, options: ARSession.RunOptions(arrayLiteral: [.resetTracking, .removeExistingAnchors]))
+      }
+    
+       override func viewWillDisappear(_ animated: Bool) {
+           super.viewWillDisappear(animated)
+
+           // Pause the view's session
+           sceneView.session.pause()
+       }
 }
 
